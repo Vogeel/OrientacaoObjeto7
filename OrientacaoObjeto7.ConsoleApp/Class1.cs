ffb3083aeb32960cf7366a3fbb81c5ff822aa427
Vogeel <vitorvogel12@gmail.com>
2022-03-08 16:12:16 -0300

    Add project files.

---
+++ OrientacaoObjeto7.ConsoleApp/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjeto7.ConsoleApp
+{
+    internal class Vendedor
+    {
+        public double salariobase;
+        public double valordevendas;
+        public double porcem;
+        public double porcentagem()
+        {
+            double calculoporcentagem = porcem / 100;
+            return calculoporcentagem;
+        }
+
+        public double comissao()
+        {
+            double ValorComissao = valordevendas * porcentagem();
+            return ValorComissao;
+        }
+        public double salariofinal()
+        {
+            double ValorFinal = comissao() + salariobase;
+            return ValorFinal;
+        }
+    }
+}
---
+++ OrientacaoObjeto7.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OrientacaoObjeto7.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vendedor vendedor = new Vendedor();
+            vendedor.salariobase = 1200;
+            Console.WriteLine("Quanto de venda o vendedor fez este mes?");
+            vendedor.valordevendas = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Qual a porcentagem de comissao do vendedor?");
+            vendedor.porcem = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Com o total de vendas de: R${vendedor.valordevendas}, o vendedor obteve um salario total com comissao de: R${vendedor.salariofinal()}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
